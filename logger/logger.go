a6fef588722a578cfea4cb851323db5214cf84ba
defskela <sashokafanas@gmail.com>
2025-01-02 20:01:16 +0300

    Сделал логгер более удобным в использовании, добавил комментарии к функциям

--- logger/logger.go
+++ logger/logger.go
@@ -22,8 +22,18 @@ type Logger struct {
 	level int
 }
 
+var defaultLogger *Logger
+
+func init() {
+	defaultLogger, _ = NewLogger(0)
+}
+
+func SetLevel(level int) {
+	defaultLogger.level = level
+}
+
 func NewLogger(level int) (*Logger, error) {
-	if level == 0 || level == 1 || level == 2 || level == 3 {
+	if level >= 0 && level <= 4 {
 		return &Logger{
 			infoLogger:  log.New(os.Stdout, "\033[1;34mINFO\033[0m: ", log.Ldate|log.Ltime),
 			errorLogger: log.New(os.Stderr, "\033[1;31mERROR\033[0m: ", log.Ldate|log.Ltime|log.Lshortfile),
@@ -32,14 +42,12 @@ func NewLogger(level int) (*Logger, error) {
 			level:       level,
 		}, nil
 	} else {
-		return nil, fmt.Errorf("Logger level must be 0 or 1 or 2 or 3 or 4")
+		return nil, fmt.Errorf("Logger level must be between 0 and 4")
 	}
-
 }
 
 func (l *Logger) Info(message ...any) {
 	if l.level < 1 {
-		// Преобразуем каждый элемент в строку и объединяем их через пробел
 		var strMessages []string
 		for _, m := range message {
 			strMessages = append(strMessages, fmt.Sprint(m))
@@ -58,7 +66,7 @@ func (l *Logger) Debug(message ...any) {
 	}
 }
 
-func (l *Logger) Warn(message ...string) {
+func (l *Logger) Warn(message ...any) {
 	if l.level < 3 {
 		var strMessages []string
 		for _, m := range message {
@@ -72,4 +80,21 @@ func (l *Logger) Error(err error) {
 	if l.level < 4 {
 		l.errorLogger.Println(err)
 	}
+}
+
+// Глобальные функции
+func Info(message ...any) {
+	defaultLogger.Info(message...)
+}
+
+func Debug(message ...any) {
+	defaultLogger.Debug(message...)
+}
+
+func Warn(message ...any) {
+	defaultLogger.Warn(message...)
+}
+
+func Error(err error) {
+	defaultLogger.Error(err)
 }
